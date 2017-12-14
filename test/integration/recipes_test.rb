6e436e5ed415bf98c894e7497e20f2ebd660d35c
OviyaMani <oviyamani1997@gmail.com>
2017-12-14 05:11:17 +0000

    Completed recipes form

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -34,16 +34,17 @@ test "should get recipes listing" do
 end
 
 test "should get recipes show" do
+
   get recipe_path(@recipe)
   assert_template 'recipes/show'
   assert_match @recipe.name, response.body
   assert_match @recipe.description, response.body
   assert_match @chef.chefname, response.body
+  assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+  assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+
 end
 
-  test "create new valid recipe" do
-    get new_recipe_path
-  end
   test "create new valid recipe" do
   get new_recipe_path
   assert_template 'recipes/new'
