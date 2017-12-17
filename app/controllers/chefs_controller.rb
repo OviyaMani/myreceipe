a3057088bcf08dce2cc25a833b59c19f10b03cf1
OviyaMani <oviyamani1997@gmail.com>
2017-12-17 16:54:04 +0000

    Last update for demo

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -3,7 +3,7 @@ class ChefsController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
-    @chefs = Chef.all
+    @chefs = Chef.paginate(page: params[:page], per_page: 2)
   end
   
   def new
